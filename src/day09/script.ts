eeb89ea2ba7ad8f57d7def4617ed4f772fe95b69
Nadine <nadlgit@users.noreply.github.com>
2023-12-09 14:58:10 -0400

    day 9

---
+++ src/day09/script.ts
@@ -0,0 +1,46 @@
+import { getInputLines, runDay } from '../utils';
+
+const exampleFilename = 'example-input.txt';
+
+runDay(
+  {
+    example: { filename: exampleFilename, expected: 114 },
+    solveFn: (filename) => sumExtrapolatedNextValues(filename),
+  },
+  {
+    example: { filename: exampleFilename, expected: 2 },
+    solveFn: (filename) => sumExtrapolatedPrevValues(filename),
+  }
+);
+
+function parseInput(filename: string) {
+  const lines = getInputLines(filename);
+  return lines.map((line) => line.split(' ').map((value) => parseInt(value)));
+}
+
+function extrapolateBoundValues(sequence: number[]) {
+  const seqBoundValues = (seq: number[]): [number, number] => [seq[0], seq[seq.length - 1]];
+  let currSeq = [...sequence];
+  const boundValues = [seqBoundValues(sequence)];
+  while (currSeq.some((n) => n !== currSeq[0])) {
+    currSeq = currSeq.reduce(
+      (acc, n, idx) => (idx > 0 ? [...acc, n - currSeq[idx - 1]] : acc),
+      [] as number[]
+    );
+    boundValues.push(seqBoundValues(currSeq));
+  }
+  boundValues.reverse();
+  const nextValue = boundValues.map(([, last]) => last).reduce((acc, n) => n + acc, 0);
+  const prevValue = boundValues.map(([first]) => first).reduce((acc, n) => n - acc, 0);
+  return { nextValue, prevValue };
+}
+
+function sumExtrapolatedNextValues(filename: string) {
+  const sequences = parseInput(filename);
+  return sequences.reduce((acc, seq) => acc + extrapolateBoundValues(seq).nextValue, 0);
+}
+
+function sumExtrapolatedPrevValues(filename: string) {
+  const sequences = parseInput(filename);
+  return sequences.reduce((acc, seq) => acc + extrapolateBoundValues(seq).prevValue, 0);
+}
